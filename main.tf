5d18862bb9f27c6668327f95f6f721d74a8d51b1
shujath-git900 <shujath.devops@gmail.com>
2026-05-19 13:26:54 +0530

    creating k8s cluster

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sh-terraform-backend-bucket "
+    key    = "kubernetes/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_eks_cluster" "main" {
+  name = "example"
+
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.35"
+
+  vpc_config {
+    subnet_ids = [ "subnet-0242f46eea951417e", "subnet-09bb41f51cb89a526" ]
+
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-main"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
