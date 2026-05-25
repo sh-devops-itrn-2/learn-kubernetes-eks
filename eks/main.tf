f3e032fbdcd79cf2102288539f6bf2a80cab6fde
shujath-git900 <shujath.devops@gmail.com>
2026-05-25 12:36:14 +0530

    added the version 1.36

--- eks/main.tf
+++ eks/main.tf
@@ -14,7 +14,7 @@ resource "aws_eks_cluster" "main" {
   name = "example"
 
   role_arn = aws_iam_role.cluster.arn
-  version  = "1.35"
+  version  = "1.36"
 
   vpc_config {
     subnet_ids = [ "subnet-0242f46eea951417e", "subnet-09bb41f51cb89a526" ]
